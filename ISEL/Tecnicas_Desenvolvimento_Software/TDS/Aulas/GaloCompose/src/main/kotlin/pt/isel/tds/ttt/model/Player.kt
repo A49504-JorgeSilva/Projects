b2e02e92ac7c9f1c96c54174091edf85695370de
49504 - Jorge Silva <A49504@alunos.isel.pt>
2025-08-11 00:57:36 +0100

    Changes on projects from class TDS

--- ISEL/Tecnicas_Desenvolvimento_Software/TDS/Aulas/GaloCompose/src/main/kotlin/pt/isel/tds/ttt/model/Player.kt
+++
@@ -1,9 +0,0 @@
-package pt.isel.tds.ttt.model
-
-/**
- * Represents a player.
- */
-enum class Player {
-    X, O;
-    val other get() = if (this==X) O else X
-}
--- ISEL/Tecnicas_Desenvolvimento_Software/TDS/Aulas/README.md
+++
@@ -1,2 +0,0 @@
-# 2324i.31d
-Código realizado nas aulas da turma LEIC31D de TDS no semestre 2023/2024 Inverno
